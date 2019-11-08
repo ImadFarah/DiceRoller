87d793307a85e9d5bc704b5f2dc1fb7f938815a6
IMAD <imadsigad@outlook.com>
2019-11-08 03:02:12 +0000

    Level 4

--- app/src/main/java/com/example/diceroller/MainActivity.java
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -56,32 +56,65 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+
+
     public void on_button_click(View view) {
 
-            int scorecount = 0;
-            int score = 0;
+        roll_Dice();
 
+        int scorecount = 0;
+        int score = 0;
+
+        TextView tv = this.findViewById(R.id.numview);
+        TextView gs = this.findViewById(R.id.guessview);
+        TextView ms = this.findViewById(R.id.messageview);
+        TextView S = this.findViewById(R.id.SCORE);
+
+        int generatednumber = Integer.valueOf(tv.getText().toString());
+        int user = Integer.valueOf(gs.getText().toString());
+
+        if (generatednumber == user) {
+            ms.setText("Congratulations");
+            score++;
+            scorecount = scorecount + score;
+
+            S.setText(Integer.toString(scorecount));
+
+        }
+    }
+        public void on_button_click2(View view) {
+
+            roll_Dice();
 
             TextView tv = this.findViewById(R.id.numview);
-            TextView gs = this.findViewById(R.id.guessview);
-            TextView ms = this.findViewById(R.id.messageview);
-            TextView S = this.findViewById(R.id.SCORE);
+            TextView msg = this.findViewById(R.id.guessview);
 
-            int generatednumber = Integer.valueOf(tv.getText().toString());
-            int user = Integer.valueOf(gs.getText().toString());
+            int newnumber = Integer.valueOf(tv.getText().toString());
+
+            if (newnumber == 1) {
+                msg.setText("If you could go anywhere in the world, where would you go?" );
+            } else if (newnumber == 2){
+                msg.setText("If you were stranded on a desert island, what three things would you want to take with you?");
+            }else if (newnumber == 3){
+                msg.setText("If you could eat only one food for the rest of your life, what would that be?");
+            }else if (newnumber == 4){
+                msg.setText("If you won a million dollars, what is the first thing you would buy?");
+            }else if (newnumber == 5){
+                msg.setText("If you could spend the day with one fictional character, who would it be?");
+            }else if (newnumber == 6){
+                msg.setText("If you found a magic lantern and a genie gave you three wishes, what would you wish?");
+            }
+        }
+
+
+        public void roll_Dice(){
+            TextView tv = this.findViewById(R.id.numview);
 
             Random numberGenerated = new Random();
 
             int number = numberGenerated.nextInt(6);
 
             tv.setText(Integer.toString(number));
-
-            if (generatednumber == user) {
-                ms.setText("Congratulations");
-                 score++;
-                 scorecount= scorecount+score;
-                S.setText(Integer.toString(number));
-            }
 
         }
 
